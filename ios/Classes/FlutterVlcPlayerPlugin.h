4e23a52fcc7cf6f54c7d5395ae64497fe3cf4bfe
Mitch Ross <mitchross@users.noreply.github.com>
2020-05-19 02:49:06 -0700

    Swift conversion v2 (#50)
    * Swift conversion
    
    * Updated example app with Play/Pause

--- ios/Classes/FlutterVlcPlayerPlugin.h
+++ ios/Classes/FlutterVlcPlayerPlugin.h
@@ -1,34 +1,4 @@
 #import <Flutter/Flutter.h>
-#import <MobileVLCKit/MobileVLCKit.h>
 
 @interface FlutterVlcPlayerPlugin : NSObject<FlutterPlugin>
-@end
-
-@interface FLTPlayerView : NSObject<FlutterPlatformView>
-
-/// View to show video over
-@property (nonatomic, strong) UIView *hostedView;
-/// Player showing video
-@property (nonatomic, strong) VLCMediaPlayer *player;
-/// result to comunicate back to Flutter
-@property (nonatomic) FlutterResult result;
-/// Set to indicate that aspect has been set which is only needed once.
-@property (nonatomic, assign) BOOL aspectSet;
-
-
-/// Initialize a new instance with the channel
-/// @param methodChannel Method channel for communication from flutter
-/// @param eventChannel Event channel for communication back to flutter
-+ (instancetype)initWithChannels: (FlutterMethodChannel*) methodChannel andEventChannel:(FlutterEventChannel*) eventChannel;
-
-@end
-
-@interface FLTPlayerViewFactory : NSObject<FlutterPlatformViewFactory>
-+ (instancetype)initWithRegistrar : (NSObject<FlutterPluginRegistrar>*)registrar;
-@end
-
-@interface FLTPlayerEventStreamHandler : NSObject<FlutterStreamHandler, VLCMediaPlayerDelegate>
-
-@property (nonatomic) FlutterEventSink eventSink;
-
 @end
