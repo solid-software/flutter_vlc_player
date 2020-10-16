25c5168fe1810e6db1e7eb5e3bf5ad771f0e87fb
mitch ross <mitchross09@gmail.com>
2020-10-16 15:01:03 -0400

    small change

--- android/src/main/java/software/solid/fluttervlcplayer/FlutterVlcPlayerPlugin.java
+++ android/src/main/java/software/solid/fluttervlcplayer/FlutterVlcPlayerPlugin.java
@@ -3,11 +3,14 @@ package software.solid.fluttervlcplayer;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** FlutterVlcPlayerPlugin */
-public class FlutterVlcPlayerPlugin implements FlutterPlugin {
+public class FlutterVlcPlayerPlugin implements FlutterPlugin, ActivityAware {
+  private FlutterPluginBinding pluginBinding;
 
   public FlutterVlcPlayerPlugin() {}
 
@@ -25,19 +28,39 @@ public class FlutterVlcPlayerPlugin implements FlutterPlugin {
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
-    BinaryMessenger messenger = binding.getBinaryMessenger();
-    binding
-            .getPlatformViewRegistry()
-            .registerViewFactory(
-                    "flutter_video_plugin/getVideoView", new FlutterVideoViewFactory(messenger, /*containerView=*/ null, binding.getTextureRegistry()));
+    this.pluginBinding = binding;
+
   }
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-
+pluginBinding= null;
 
 
   }
 
 
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+    BinaryMessenger messenger = pluginBinding.getBinaryMessenger();
+    pluginBinding
+            .getPlatformViewRegistry()
+            .registerViewFactory(
+                    "flutter_video_plugin/getVideoView", new FlutterVideoViewFactory(messenger, /*containerView=*/ null, pluginBinding.getTextureRegistry()));
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+
+  }
 }
