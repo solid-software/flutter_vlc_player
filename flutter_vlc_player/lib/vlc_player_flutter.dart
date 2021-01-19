5c62767f2ff723b321e4733ba6d05aa0ae3b3ec7
alr2413 <alr2413@gmail.com>
2021-01-19 22:03:28 +0330

    fix export libary

--- flutter_vlc_player/lib/vlc_player_flutter.dart
+++ flutter_vlc_player/lib/vlc_player_flutter.dart
@@ -1,4 +1,10 @@
-library flutter_vlc_player;
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+export 'src/enums/playing_state.dart' show PlayingState;
+export 'src/vlc_player_controller.dart' show VlcPlayerController;
+export 'src/flutter_vlc_player.dart' show VlcPlayer;
+export 'src/vlc_player_value.dart' show VlcPlayerValue;
 
 export 'package:flutter_vlc_player_platform_interface/vlc_player_flutter_platform_interface.dart'
     show
@@ -14,7 +20,3 @@ export 'package:flutter_vlc_player_platform_interface/vlc_player_flutter_platfor
         VlcRtpOptions,
         VlcStreamOutputOptions,
         VlcVideoOptions;
-
-export 'src/enums/playing_state.dart';
-export 'src/vlc_player_controller.dart';
-export 'src/flutter_vlc_player.dart';
