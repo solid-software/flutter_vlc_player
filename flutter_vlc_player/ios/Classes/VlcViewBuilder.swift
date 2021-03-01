8c14e6991c5cce2a9408533f5e4ea6f756dffb95
alr2413 <alr2413@gmail.com>
2021-03-02 00:47:25 +0330

    split swift code to multiple files

---
+++ flutter_vlc_player/ios/Classes/VlcViewBuilder.swift
@@ -0,0 +1,8 @@
+//
+//  VlcViewBuilder.swift
+//  flutter_vlc_player
+//
+//  Created by alireza setayesh on 3/2/21.
+//
+
+import Foundation
---
+++ flutter_vlc_player/ios/Classes/VlcViewController.swift
@@ -0,0 +1,8 @@
+//
+//  VlcViewController.swift
+//  flutter_vlc_player
+//
+//  Created by alireza setayesh on 3/2/21.
+//
+
+import Foundation
