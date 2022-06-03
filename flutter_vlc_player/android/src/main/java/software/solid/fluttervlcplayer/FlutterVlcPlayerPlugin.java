74d26a600c7f2830ff996d3d77914cc00d375fad
Ian Koerich Maciel <ianmaciel@gmail.com>
2022-06-03 12:14:47 -0300

    Fix plugin destructor (#332)
    Fixes #237

--- flutter_vlc_player/android/src/main/java/software/solid/fluttervlcplayer/FlutterVlcPlayerPlugin.java
+++ flutter_vlc_player/android/src/main/java/software/solid/fluttervlcplayer/FlutterVlcPlayerPlugin.java
@@ -105,7 +105,9 @@ public class FlutterVlcPlayerPlugin implements FlutterPlugin, ActivityAware {
     }
 
     private static void stopListening() {
-        if (flutterVlcPlayerFactory != null)
+        if (flutterVlcPlayerFactory != null) {
             flutterVlcPlayerFactory.stopListening();
+            flutterVlcPlayerFactory = null;
+        }
     }
 }
